ff7db3329768a414b88c8d4cecc47404016c35c8
nicolaxe <109052896+nicolaxe@users.noreply.github.com>
2026-04-01 18:31:04 +0200

    Tolerate stupid Range requests because DLNA is a mess

--- dlnactl/server.py
+++ dlnactl/server.py
@@ -2,6 +2,7 @@ import socket
 import mimetypes
 import magic
 import logging
+import os
 
 from aiohttp import web
 from aiohttp.web_request import Request
@@ -16,6 +17,42 @@ def get_local_ip() -> str:
     s.close()
     return ip
 
+# Some slop code that "correctly" handles devices requesting stupid HTTP Ranges
+# By "correctly" i mean it just accepts and sends back what the device expects, standards be damned
+def get_file_response(path: Path, range_header: str, content_type: str):
+    file_size = os.path.getsize(path)
+    units, rng = range_header.split('=')
+    start_str, end_str = rng.split('-')
+
+    start = int(start_str)
+    end = int(end_str) if end_str else file_size - 1
+
+    if start >= file_size:
+        logger.warning(f'Invalid HTTP Range header: {start} for "{path}" (size={file_size}), resetting to 0')
+        start = 0
+        end = file_size - 1
+
+    end = min(end, file_size - 1)
+    length = end - start + 1
+
+    headers = {
+            'content-type': content_type,
+            'Accept-Ranges': 'bytes',
+            'transferMode.dlna.org': 'Streaming',
+            'Content-Range': f'bytes {start}-{end}/{file_size}',
+            'Content-Length': str(length)
+    }
+
+    with open(path, 'rb') as f:
+        f.seek(start)
+        data = f.read(length)
+
+    return web.Response(
+        status=206,
+        body=data,
+        headers=headers,
+        )
+
 
 
 class DLNAServer:
@@ -27,16 +64,33 @@ class DLNAServer:
         path = self.paths.get(request.path)
         if path is None:
             return web.Response(status=404)
-        # For whatever reason aiohttp often fails to auto-detect the MIME type. I have to set it manually
+
+        # MIME detection
         content_type = magic.from_file(path, mime=True)
-        
+
         if content_type == 'application/octet-stream':
             logger.warning(f'Unable to determine filetype of "{path}". Using the file extension')
             content_type = mimetypes.guess_type(path)[0]
             if content_type is None:
                 content_type = 'application/octet-stream'
 
-        headers = {'content-type': content_type}
+
+        headers = {
+            'content-type': content_type,
+            'Accept-Ranges': 'bytes',
+            'transferMode.dlna.org': 'Streaming',
+        }
+
+        range_header = request.headers.get('Range')
+
+        if range_header:
+            try:
+                return get_file_response(path, range_header, content_type)
+            except Exception as e:
+                logger.exception(f'Bad range request: {range_header} ({e})')
+                return web.Response(status=416)
+
+
         return web.FileResponse(path, headers=headers)
 
     async def start_server(self):
